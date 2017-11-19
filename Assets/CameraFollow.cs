ef8279919c8bdf8611efc5999be63e4dab5072d8
Voxel-Bits <seekanddestroy5903@yahoo.com>
2017-11-18 18:48:53 -0800

    Camera following cube!
    Now to get it to zoom in and out

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;  
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    //Target's position will be updated in 'Updated' func, updating that and camera position will cause jitteriness
+
+    void LateUpdate() //run right after update
+    {
+
+        transform.position = target.position + offset;
+    }
+
+    //Maybe I don't need a camera follow really?? RAther I don't need an offset!
+
+    //Now to 
+}
